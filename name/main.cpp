a6f1c451247b58a24d94f3f5c95d9ebd20166b7a
Shiv0130 <159130557+Shiv0130@users.noreply.github.com>
2026-08-13 19:06:01 +0200

    Add files via upload

---
+++ name/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+using namespace std;
+
+/* Write a c++ program that prints out "access granted " if the user name is ur name and the password is = 12345
+
+Access is only given if the user is above 18yrs of age
+Write a c++ program that prints out access granted if conditions are met otherwise access is denied
+ */
+
+int main()
+{
+    string name;
+    string password;
+    int age;
+
+
+    cout << "Enter name:";
+    cin>>name;
+
+    cout << "Enter password:";
+    cin>>password;
+
+    cout << "Enter age:";
+    cin>>age;
+
+    if(name=="Shivaar" & age>=18 & password="1234"){
+        cout<<"Access granted"<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
+#include <iostream>
+
+using namespace std;
+
+/* Write a c++ program that prints out "access granted " if the user name is ur name and the password is = 12345
+
+Access is only given if the user is above 18yrs of age
+Write a c++ program that prints out access granted if conditions are met otherwise access is denied
+ */
+
+int main()
+{
+    string name;
+    string password;
+    int age;
+
+
+    cout << "Enter name:";
+    cin>>name;
+
+    cout << "Enter password:";
+    cin>>password;
+
+    cout << "Enter age:";
+    cin>>age;
+
+    if(name=="Shivaar" & age>=18 & password="1234"){
+        cout<<"Access granted"<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
+#include <iostream>
+
+using namespace std;
+
+/* Write a c++ program that prints out "access granted " if the user name is ur name and the password is = 12345
+
+Access is only given if the user is above 18yrs of age
+Write a c++ program that prints out access granted if conditions are met otherwise access is denied
+ */
+
+int main()
+{
+    string name;
+    string password;
+    int age;
+
+
+    cout << "Enter name:";
+    cin>>name;
+
+    cout << "Enter password:";
+    cin>>password;
+
+    cout << "Enter age:";
+    cin>>age;
+
+    if(name=="Shivaar" & age>=18 & password="1234"){
+        cout<<"Access granted"<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
